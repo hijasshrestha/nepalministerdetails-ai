d058aa8b3cb5c88bc358375865ba670edfa5e540
Hijas Shrestha <31115358+hijasshrestha@users.noreply.github.com>
2026-03-28 18:15:36 +1100

    Add files via upload

---
+++ data/ministermap.js
@@ -0,0 +1,17 @@
+export const ministerMap = {
+  "Prime Minister and Ministry of Defence and Industry": "Balendra Shah",
+  "Ministry of Finance": "Dr. Swarnim Wagle",
+  "Ministry of Home Affairs": "Sudan Gurung",
+  "Ministry of Foreign Affairs": "Shishir Khanal",
+  "Ministry of Physical Infrastructure": "Sunil Lamsal",
+  "Ministry of Energy": "Biraj Bhakta Shrestha",
+  "Ministry of Tourism": "Khadak Raj Poudel",
+  "Ministry of Education": "Sasmit Pokharel",
+  "Ministry of Health": "Nisha Mehta",
+  "Ministry of Communications": "Dr. Bikram Timilsina",
+  "Ministry of General Administration": "Pratibha Rawal",
+  "Ministry of Law": "Sobita Gautam",
+  "Ministry of Agriculture": "Geeta Chaudhary",
+  "Ministry of Labour": "Deepak Kumar Sah",
+  "Ministry of Women and Children": "Sita Badi"
+};
